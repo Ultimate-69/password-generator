99f4d0e9830ae0ec48ffc0fd90cb21f1c2d4e4f5
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-20 15:29:54 +0200

    getting workign random numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
+#include <random>
 #include <iostream>
 
 int main()
 {
-    
+    const char* letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 51); // Get random number in alphabet's range
+
+    std::cout << letters[dist6(rng)] << std::endl;
 }
