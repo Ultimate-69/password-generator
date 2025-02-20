82e297044315672b43abe248c71d188801dbde41
Ultimate-69 <170136337+Ultimate-69@users.noreply.github.com.>
2025-02-20 15:40:02 +0200

    ask for password length and provide a password based on that

--- main.cpp
+++ main.cpp
@@ -4,9 +4,47 @@
 int main()
 {
     const char* letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const char* symbols = "!@#$%&*?";
+    const char* numbers = "0123456789";
+
     std::random_device dev;
     std::mt19937 rng(dev());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 51); // Get random number in alphabet's range
+    std::uniform_int_distribution<std::mt19937::result_type> randomLetter(0, 51);
+    std::uniform_int_distribution<std::mt19937::result_type> randomSymbol(0, 7);
+    std::uniform_int_distribution<std::mt19937::result_type> randomNumber(0, 9);
+    std::uniform_int_distribution<std::mt19937::result_type> iType(0, 10);
 
-    std::cout << letters[dist6(rng)] << std::endl;
+    int length = 0;
+
+    std::cout << "Enter the password length: ";
+    try
+    {    
+        std::cin >> length;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "An error was encountered when getting the password length.";
+    }
+
+    std::string password;
+
+    for (int i = 0; i < length; i++)
+    {
+       int typeOfLetter = iType(rng);
+       if (typeOfLetter <= 5)
+       {
+            password += letters[randomLetter(rng)];
+       }
+       else if (typeOfLetter <= 8)
+       {
+            password += numbers[randomNumber(rng)];
+       }
+       else if (typeOfLetter <= 10)
+       {
+            password += symbols[randomSymbol(rng)];
+       }
+
+    }
+
+    std::cout << "Your password: " << password << std::endl;
 }
